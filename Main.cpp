84ae8b92f97b26210ca47bc50830a44cb9ea365e
BigSirj1993 <77455531+BigSirj1993@users.noreply.github.com>
2021-02-08 16:37:26 -0600

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+#include "Person.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+	int name;
+	int age;
+
+	cout << "Thank you for taking your time to do this survey. Please enter your name:" << endl;
+	cin >> name;
+	cout << "Now please enter your age: " << endl;
+	cin >> age;
+
+
+
+
+
+	system("pause");
+	return 0;
+
+
+}
