51da77fdb3c5ffae66c6ed028e19d368017c5b7d
BigSirj1993 <77455531+BigSirj1993@users.noreply.github.com>
2021-02-10 14:47:01 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,10 @@
+#include "GamingStudent.h"
+#include "NonGamingStudent.h"
+#include "Survey.h"
+#include "Student.h"
 #include "Person.h"
 #include <iostream>
+
 
 using namespace std;
 
@@ -7,16 +12,59 @@ using namespace std;
 
 int main()
 {
-	int name;
-	int age;
+	// Creating our classes
+	Survey mySurvey;
+	Student myStudent;
+	myStudent.createRandomStudent();
+	myStudent.printInfo();
 
-	cout << "Thank you for taking your time to do this survey. Please enter your name:" << endl;
-	cin >> name;
-	cout << "Now please enter your age: " << endl;
-	cin >> age;
+	// Accessing info from our classes
+	cout << myStudent.getName() << endl;
+	cout << myStudent.getAge() << endl;
+	cout << myStudent.getCollege() << endl;
+	cout << myStudent.getProgramName() << endl;
+	cout << myStudent.getCurrentSemester() << endl;
+	
+	
+	// Mutating info in our myStudent
+	myStudent.setName("Nick");
+	myStudent.setAge('27');
+	myStudent.setCollege("George Brown");
+	myStudent.setProgramName("Video Game Programming");
+	myStudent.setCurrentSemester('2');
 
 
+	// Getting info to check if mutation worked
 
+	cout << myStudent.getName() << endl;
+	cout << myStudent.getAge() << endl;
+	cout << myStudent.getCollege() << endl;
+	cout << myStudent.getProgramName() << endl;
+	cout << myStudent.getCurrentSemester() << endl;
+
+	// Mutating info in mySurvey
+	mySurvey.setName("Nick");
+	mySurvey.setAge('27');
+	mySurvey.setCollege("George Brown");
+	mySurvey.setProgramName("");
+	mySurvey.setCurrentSemester(2);
+
+	//Get info again(Getter)
+	cout << mySurvey.getName("Nick") << endl;
+	cout << mySurvey.getAge('27') << endl;
+	cout << mySurvey.getCollege("George Brown") << endl;
+	cout << mySurvey.getProgramName("") << endl;
+	cout << mySurvey.getCurrentSemester('2') << endl;
+
+	/*for (int i = 0;i < 500; i++)
+	{
+		if (myStudent)
+		{
+
+		
+		}
+
+	}*/
 
 
 	system("pause");
