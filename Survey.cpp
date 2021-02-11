09519e2f3f53609fca59d8863a81adedd0e76ffb
BigSirj1993 <77455531+BigSirj1993@users.noreply.github.com>
2021-02-11 13:28:54 -0600

    Update Survey.cpp

--- Survey.cpp
+++ Survey.cpp
@@ -1,6 +1,8 @@
 #include "Survey.h"
 #include <string>
 #include <iostream>
+#include "GamingStudent.h"
+#include "NonGamingStudent.h"
 
 using namespace std;
 
@@ -10,24 +12,96 @@ Survey::Survey()
 	numberOfGamingStudents = 0;
 	totalNumberOfGamingHours = 0;
 	averageNumberOfGamingHours = 0;
+	totalNumberOfNonGamingHours = 0;
+	averageNumberOfNonGamingHours = 0;
+	totalAgeOfGamingStudents = 0;
+	totalAgeOfNonGamingStudents = 0;
+	mostPreferedDevice = "";
 	mostPreferedService = "";
 
 };
 
 void Survey::process() 
 {
+	int numberOfStudentWhoPlayXbox = 0;
+	int numberOfStudentWhoPlayPlayStation = 0;
+	int numberOfStudentWhoWatchNexflix = 0;
+	int numberOfStudentWhoWatchFunimation = 0;
 	for (int i = 0;i < 500; i++) // the for loop takes one variable [i] starts at 0, then goes to 499
 	{
-		studentArray[i].printInfo();
+		//studentArray[i]->printInfo();
+		if (studentArray[i]->getIsGamingStudent())
+		{
+			numberOfGamingStudents++;
+			totalNumberOfGamingHours += static_cast<GamingStudent*>(studentArray[i])->getHoursOfGameFun(); 
+			string gamingDevice = static_cast<GamingStudent*>(studentArray[i])->getGamingDevice();
+			if (gamingDevice == "Xbox")
+			{
+				numberOfStudentWhoPlayXbox++;
+			}
+			else if (gamingDevice == "PlayStation")
+			{
+				numberOfStudentWhoPlayPlayStation++;
+			}
+			
+		}
+		else if(studentArray[i]->getIsNotGamingStudent())
+		{
+			numberOfNonGamingStudents++;
+			totalNumberOfNonGamingHours += static_cast<NonGamingStudent*>(studentArray[i])->getHoursOfNonGameFun();
+			string streamingService = static_cast<NonGamingStudent*>(studentArray[i])->getStreamingService();
+		}
+
 	}
-								
-								// Next Step: creating a list of 500 students without having to type 500 students and putting it in my main.cpp
+	if(numberOfNonGamingStudents != 0)
+	averageNumberOfNonGamingHours = totalNumberOfNonGamingHours / numberOfNonGamingStudents;
+	if (numberOfGamingStudents != 0)
+		averageNumberOfGamingHours = totalNumberOfGamingHours / numberOfGamingStudents;
+	if (numberOfStudentWhoPlayPlayStation > numberOfStudentWhoPlayXbox)
+	{
+		mostPreferedDevice = "PlayStation";
+	}
+	else if (numberOfStudentWhoPlayXbox > numberOfStudentWhoPlayPlayStation)
+	{
+		mostPreferedDevice = "Xbox";
+	}
+	else
+	{
+		mostPreferedDevice = "It's a tie";
+	}
+	if (numberOfStudentWhoWatchNexflix > numberOfStudentWhoWatchFunimation)
+	{
+		mostPreferedService = "Nextflix";
+	}
+	else if (numberOfStudentWhoWatchFunimation > numberOfStudentWhoWatchNexflix)
+	{
+		mostPreferedService = "Funimation";
+	}
+	else
+	{
+		mostPreferedService = "Its a tie";
+	}
+	cout << numberOfNonGamingStudents << endl;
+	cout << numberOfGamingStudents << endl;
+	cout << totalNumberOfGamingHours << endl;
+	cout << averageNumberOfGamingHours << endl;
+	cout << totalNumberOfNonGamingHours << endl;
+	cout << averageNumberOfNonGamingHours << endl;
+	cout << totalAgeOfGamingStudents << endl;
+	cout << totalAgeOfNonGamingStudents << endl;
+	cout << mostPreferedDevice << endl;
+	cout << mostPreferedService << endl;
 	
 }
 
-void Survey::addStudent(int _AddStudent)
+/*void Survey::addStudent(int _AddStudent)
 {
 	Student classroom;
 	classroom.createRandomStudent();
 	studentArray[_AddStudent] = classroom;
+}*/
+
+void Survey::addStudent(int _AddStudent, Student* newStudent)
+{
+	studentArray[_AddStudent] = newStudent;
 }
